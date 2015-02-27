20146be67a41f11dd55298d26053f6f679667396
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-02-27 16:11:59 +0100

    add read_specieslist()

---
+++ R/read_specieslist.R
@@ -0,0 +1,28 @@
+#' Read the species list and their constraints
+#' @export
+#'@importFrom n2khelper read_delim_git
+#'@importFrom RODBC sqlQuery odbcClose
+read_specieslist <- function(){
+  species.constraint <- read_delim_git(file = "soorttelling.txt", path = "watervogel/attribuut")
+  channel <- connect_watervogel()
+  sql <- "
+    SELECT
+      EuringCode AS SpeciesID,
+      NaamWetenschappelijk AS Species,
+      NaamNederlands AS SpeciesNL,
+      NaamEngels AS SpeciesEN
+    FROM
+      tblSoort
+  "
+  species <- sqlQuery(channel = channel, query = sql)
+  odbcClose(channel)
+  # restrict the species list to the species with constraints
+  species.constraint <- merge(species.constraint, species[, c("SpeciesID", "SpeciesNL")])
+  species.constraint$SpeciesNL <- NULL
+  species <- species[species$SpeciesID %in% species.constraint$SpeciesID, ]
+  
+  return(
+    species = species,
+    species.constraint = species.constraint
+  )
+}
