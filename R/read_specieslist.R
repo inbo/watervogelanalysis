4f154be494e9421ac9118b8e4e91d6125a62aef2
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-03-02 12:05:18 +0100

    adding example to read_specieslist()

--- R/read_specieslist.R
+++ R/read_specieslist.R
@@ -2,6 +2,10 @@
 #' @export
 #'@importFrom n2khelper read_delim_git
 #'@importFrom RODBC sqlQuery odbcClose
+#'@examples
+#'species.list <- read_specieslist()
+#'head(species.list$species)
+#'head(species.list$species.constraint)
 read_specieslist <- function(){
   species.constraint <- read_delim_git(file = "soorttelling.txt", path = "watervogel/attribuut")
   channel <- connect_watervogel()
