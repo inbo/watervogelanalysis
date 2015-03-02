77ed4c648735bcf67a6ceae060b81ecde4e3edf3
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-03-02 11:41:29 +0100

    add read_location()

---
+++ R/read_location.R
@@ -0,0 +1,21 @@
+#' read the dataset of locations from the database
+#' @export
+#' @importFrom RODBC sqlQuery odbcClose
+read_location <- function(){
+  channel <- connect_watervogel()
+  sql <- "
+    SELECT
+      Code AS LocationID,
+      Gebiedsnaam AS LocationName,
+      BeginDatum AS StartDate,
+      EindDatum AS EndDate,
+      EgVogelrichtlijngebied AS SPA
+    FROM
+      tblGebied
+  "
+  location <- sqlQuery(channel = channel, query = sql, stringsAsFactors = FALSE)
+  odbcClose(channel)
+  location$SPA[is.na(location$SPA)] <- 0
+  location$SPA <- location$SPA == -1
+  return(location)
+}
