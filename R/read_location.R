3cb90bb3e2fe9f60b69ce4a4896f93edcd9aa6f0
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-03-05 15:08:40 +0100

    read_location() combines Flemish and Walloon locations

--- R/read_location.R
+++ R/read_location.R
@@ -24,5 +24,20 @@ read_location <- function(){
   odbcClose(channel)
   location$SPA[is.na(location$SPA)] <- 0
   
+  walloon.location <- read_delim_git(file = "location.txt", path = "watervogel/wallonia")
+  if(class(walloon.location) == "logical"){
+    return(location)
+  }
+  if(any(location$LocationID %in% walloon.location$LocationID)){
+    stop("Identical id's in Flemish and Walloon locations")
+  }
+  
+  walloon.location <- walloon.location[, c("LocationID", "LocationName", "SPA")]
+  walloon.location$StartDate <- NA
+  walloon.location$EndDate <- NA
+  
+  location <- rbind(location, walloon.location)
+  location <- location[order(location$LocationID), ]
+  
   return(location)
 }
