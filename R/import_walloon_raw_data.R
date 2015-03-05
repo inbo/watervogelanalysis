f79e762cc6bea2424188ecc67846978c9d5f20a0
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-03-05 16:05:56 +0100

    bugfix: character to date convertion

--- R/import_walloon_raw_data.R
+++ R/import_walloon_raw_data.R
@@ -69,6 +69,7 @@ import_walloon_raw_data <- function(
   }
   visit <- visit[, old.names]
   colnames(visit) <- new.names
+  visit$Date <- as.Date(visit$Date, format = "%d/%M/%Y")
   if(any(duplicated(visit$ObservationID))){
     warning("duplicate id in ", visit.file)
     visit <- aggregate(
