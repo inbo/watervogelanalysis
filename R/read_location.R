bae1010d44fb576612270f1c3111087dac1e4d40
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-04-08 18:19:06 +0200

    update import of locations to structure of results database

--- R/read_location.R
+++ R/read_location.R
@@ -1,17 +1,22 @@
 #' read the dataset of locations from the database
 #' @inheritParams n2khelper::odbc_connect
 #' @export
-#' @importFrom n2khelper odbc_connect
+#' @importFrom n2khelper check_single_logical odbc_connect get_datasource_id git_connect
 #' @importFrom RODBC sqlQuery odbcClose
 #' @examples
 #' location <- read_location()
 #' head(location)
 read_location <- function(develop = TRUE){
-  channel <- odbc_connect(data.source = "Raw data watervogels Flanders", develop = develop)
+  develop <- check_single_logical(develop)
+  
+  # read Flemisch data from the database
+  data.source.name <- "Raw data watervogels Flanders"
+  data.source.id <- get_datasource_id(data.source.name = data.source.name)
+  channel <- odbc_connect(data.source.name = data.source.name, develop = develop)
   sql <- "
     SELECT
-      Code AS LocationID,
-      Gebiedsnaam AS LocationName,
+      Code AS ExternalCode,
+      Gebiedsnaam AS Description,
       BeginDatum AS StartDate,
       EindDatum AS EndDate,
       ABS(EgVogelrichtlijngebied) AS SPA
@@ -24,22 +29,27 @@ read_location <- function(develop = TRUE){
   "
   location <- sqlQuery(channel = channel, query = sql, stringsAsFactors = FALSE)
   odbcClose(channel)
+  location$DatasourceID <- data.source.id
   location$SPA[is.na(location$SPA)] <- 0
   
-  walloon.location <- read_delim_git(file = "location.txt", path = "watervogel/wallonia")
+  # Read Walloon data from the git repository
+  data.source.name <- "Raw data watervogels Wallonia"
+  data.source.id <- get_datasource_id(data.source.name = data.source.name)
+  path <- git_connect(data.source.name = data.source.name, develop = develop)
+  walloon.location <- read_delim_git(file = "location.txt", path = path)
   if(class(walloon.location) == "logical"){
+    Encoding(location$Description) <- "UTF-8"
     return(location)
   }
-  if(any(location$LocationID %in% walloon.location$LocationID)){
-    stop("Identical id's in Flemish and Walloon locations")
-  }
-  
   walloon.location <- walloon.location[, c("LocationID", "LocationName", "SPA")]
+  colnames(walloon.location) <- c("ExternalCode", "Description", "SPA")  
+  walloon.location$SPA[is.na(walloon.location$SPA)] <- 0
   walloon.location$StartDate <- NA
   walloon.location$EndDate <- NA
+  walloon.location$DatasourceID <- data.source.id
   
   location <- rbind(location, walloon.location)
-  location <- location[order(location$LocationID), ]
-  
+  Encoding(location$Description) <- "UTF-8"
+
   return(location)
 }
