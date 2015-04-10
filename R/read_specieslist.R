d51c4b1647d9b3c903c4b67e83583a250fba3977
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-04-10 12:21:24 +0200

    change variable names to match results database

--- R/read_specieslist.R
+++ R/read_specieslist.R
@@ -2,44 +2,51 @@
 #' @param limit Return only species with explicit constraints (default = TRUE). Otherwise return all species in the database
 #' @inheritParams n2khelper::odbc_connect
 #' @export
-#'@importFrom n2khelper read_delim_git
+#'@importFrom n2khelper check_single_logical odbc_connect read_delim_git
 #'@importFrom RODBC sqlQuery odbcClose
 #'@examples
 #'species.list <- read_specieslist()
 #'head(species.list$species)
 #'head(species.list$species.constraint)
 read_specieslist <- function(limit = TRUE, develop = TRUE){
-  species.constraint <- read_delim_git(file = "soorttelling.txt", path = "watervogel/attribuut")
-  channel <- odbc_connect(data.source.name = "Raw data watervogels Flanders", develop = develop)
   limit <- check_single_logical(limit)
   
+  channel <- odbc_connect(
+    data.source.name = "Raw data watervogels Flanders", develop = develop
+  )
   sql <- "
     SELECT
-      EuringCode AS SpeciesID,
-      NaamWetenschappelijk AS Species,
-      NaamNederlands AS SpeciesNL,
-      NaamEngels AS SpeciesEN
+      EuringCode AS ExternalCode,
+      NaamWetenschappelijk AS ScientificName,
+      NaamNederlands AS DutchName,
+      NaamEngels AS EnglishName,
+      NaamFrans AS FrenchName
     FROM
       tblSoort
   "
   species <- sqlQuery(channel = channel, query = sql, stringsAsFactors = FALSE)
   odbcClose(channel)
+  
   # restrict the species list to the species with constraints
-  species.constraint <- merge(species.constraint, species[, c("SpeciesID", "SpeciesNL")])
-  species.constraint$SpeciesNL <- NULL
-  species.constraint <- species.constraint[
-    order(species.constraint$SpeciesID, species.constraint$SpeciesCovered), 
-  ]
+  species.constraint <- read_delim_git(
+    file = "soorttelling.txt", path = "watervogel/attribuut"
+  )
+  colnames(species.constraint)[1] <- "DutchName"
+
+  species.constraint <- merge(species.constraint, species[, c("ExternalCode", "DutchName")])
+  species.constraint$DutchName <- NULL
   
   if(limit){
-    species <- species[species$SpeciesID %in% species.constraint$SpeciesID, ]
+    species <- species[species$ExternalCode %in% species.constraint$ExternalCode, ]
   }
-  species <- species[order(species$SpeciesID), ]
 
   return(
     list(
       species = species,
-      species.constraint = species.constraint[, c("SpeciesID", "Firstyear", "SpeciesCovered")]
+      species.constraint = species.constraint[
+        , 
+        c("ExternalCode", "Firstyear", "SpeciesCovered")
+      ]
     )
   )
 }
