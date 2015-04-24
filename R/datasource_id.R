e6bd73c86bc4dc8a526794f2e38e4ac3dc17b202
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-04-24 14:27:34 +0200

    add datasource_id_xxxx()

---
+++ R/datasource_id.R
@@ -0,0 +1,40 @@
+#' Get the id of the data source for the Walloon source data
+#' @inheritParams connect_flemish_source
+#' @importFrom n2khelper odbc_get_id
+#' @export
+datasource_id_wallonia <- function(result.channel){
+  odbc_get_id(
+    table = "Datasource",
+    variable = "Description",
+    value = "Source data watervogels Wallonia",
+    channel = result.channel
+  )
+}
+
+#' Get the id of the data source for the Flemish source data
+#' @inheritParams connect_flemish_source
+#' @importFrom n2khelper odbc_get_id
+#' @export
+datasource_id_flanders <- function(result.channel){
+  odbc_get_id(
+    table = "Datasource",
+    variable = "Description",
+    value = "Source data watervogels Flanders",
+    channel = result.channel
+  )
+}
+
+#' Get the id of the data source for the raw data
+#' 
+#' The raw data is the combination of the Flemish and Walloon source data
+#' @inheritParams connect_flemish_source
+#' @importFrom n2khelper odbc_get_id
+#' @export
+datasource_id_raw <- function(result.channel){
+  odbc_get_id(
+    table = "Datasource",
+    variable = "Description",
+    value = "Raw data watervogels",
+    channel = result.channel
+  )
+}
