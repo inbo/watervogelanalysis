ac088c05a776c0976812ed205e3edcefe9529a35
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-04-24 14:30:11 +0200

    split connect_source() into connect_flemish_source() and connect_walloon_source()

--- R/connect_source.R
+++ R/connect_source.R
@@ -1,9 +1,23 @@
-#' Make an ODBC connection to the source data
+#' Make an ODBC connection to the Flemish source data
 #' @export
 #' @param result.channel An open RODBC connection to the results database
 #' @importFrom n2khelper odbc_connect
-connect_source <- function(result.channel){
+connect_flemish_source <- function(result.channel){
   odbc_connect(
     data.source.name = "Source data watervogels Flanders", channel = result.channel
+  )
+}
+
+#' Make a git connection to the Walloon source data
+#' @export
+#' @inheritParams connect_flemish_source
+#' @inheritParams n2khelper::git_connection
+#' @importFrom n2khelper git_connect
+connect_walloon_source <- function(result.channel, username, password){
+  git_connect(
+    data.source.name = "Source data watervogels Wallonia", 
+    channel = result.channel,
+    username = username,
+    password = password
   )
 }
