79b15041d81b40850c49f74a2bb2f0563f1000e8
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-04-24 15:07:40 +0200

    add script to import Walloon source data

---
+++ inst/import_walloon_source.R
@@ -0,0 +1,13 @@
+library(watervogelanalysis)
+walloon.connection <- connect_walloon_source(
+  result.channel = n2khelper::connect_result(develop = TRUE), 
+  username = "thierry_onkelinx", 
+  password = 
+)
+duplicates <- import_walloon_source_data(
+  location.file = "rStation.csv", 
+  visit.file = "rVisits.csv", 
+  data.file = "rData.csv", 
+  path = ".", 
+  walloon.connection = walloon.connection
+)
