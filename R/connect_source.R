f58ae52ddc7dae4d471c58517376e46c6717f56e
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-04-24 14:52:08 +0200

    add connect_attribute()

--- R/connect_source.R
+++ R/connect_source.R
@@ -21,3 +21,17 @@ connect_walloon_source <- function(result.channel, username, password){
     password = password
   )
 }
+
+#' Make a git connection to the attribute
+#' @export
+#' @inheritParams connect_flemish_source
+#' @inheritParams n2khelper::git_connection
+#' @importFrom n2khelper git_connect
+connect_attribute <- function(result.channel, username, password){
+  git_connect(
+    data.source.name = "Attributes watervogels", 
+    channel = result.channel,
+    username = username,
+    password = password
+  )
+}
