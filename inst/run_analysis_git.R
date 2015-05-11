d50d2d7ac2ffbca4344fd1547a3d5cbfddaf86d5
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-05-12 00:08:03 +0200

    bugfix: name the n2khelper package

--- inst/run_analysis_git.R
+++ inst/run_analysis_git.R
@@ -1,7 +1,7 @@
 library(watervogelanalysis)
 prepare_analysis(
   analysis.path = "~/analysis", 
-  raw.connection = git_connection(
+  raw.connection = n2khelper::git_connection(
     repo.path = "~/rawdata",
     local.path = "watervogel",
     key = "~/.ssh/id_rsa_n2kreadonly"
