52dc81b71a596645756b7b77986b2393dd62ccc4
ThierryO <Thierry.Onkelinx@inbo.be>
2016-07-05 15:49:48 +0200

    update datasource_id_wallonia(), datasource_id_flanders() and datasource_id_raw() to work with the PostgreSQL database

--- R/datasource_id.R
+++ R/datasource_id.R
@@ -4,8 +4,8 @@
 #' @export
 datasource_id_wallonia <- function(result.channel){
   odbc_get_id(
-    table = "Datasource",
-    variable = "Description",
+    table = "datasource",
+    variable = "description",
     value = "Source data watervogels Wallonia",
     channel = result.channel
   )
@@ -17,23 +17,23 @@ datasource_id_wallonia <- function(result.channel){
 #' @export
 datasource_id_flanders <- function(result.channel){
   odbc_get_id(
-    table = "Datasource",
-    variable = "Description",
+    table = "datasource",
+    variable = "description",
     value = "Source data watervogels Flanders",
     channel = result.channel
   )
 }
 
 #' Get the id of the data source for the raw data
-#' 
+#'
 #' The raw data is the combination of the Flemish and Walloon source data
 #' @inheritParams connect_flemish_source
 #' @importFrom n2khelper odbc_get_id
 #' @export
 datasource_id_raw <- function(result.channel){
   odbc_get_id(
-    table = "Datasource",
-    variable = "Description",
+    table = "datasource",
+    variable = "description",
     value = "Raw data watervogels",
     channel = result.channel
   )
