f1f7a2235d0f626c437fc376533b0b6e398d5a80
ThierryO <Thierry.Onkelinx@inbo.be>
2017-11-17 10:25:32 +0100

    add longterm trend (full length of dataset)

--- R/prepare_analysis_model.R
+++ R/prepare_analysis_model.R
@@ -61,5 +61,48 @@ prepare_analysis_model <- function(
       )
     }
   )
-  bind_rows(yearly)
+  if (verbose) {
+    message("Longterm trend")
+  }
+  longterm <- lapply(
+    seq_along(aggregation$FileFingerprint),
+    function(i){
+      if (verbose) {
+        message("  ", aggregation[i, "FileFingerprint"])
+      }
+      object <- n2k_model_imputed(
+        result.datasource.id = aggregation[i, "ResultDatasourceID"],
+        scheme.id = aggregation[i, "SchemeID"],
+        species.group.id = aggregation[i, "SpeciesGroupID"],
+        location.group.id = aggregation[i, "LocationGroupID"],
+        model.type = "longterm imputed trend: Total ~ Year + fMonth",
+        formula = "~ cYear + f(fMonth, model = \"iid\")",
+        first.imported.year = aggregation[i, "FirstImportedYear"],
+        last.imported.year = aggregation[i, "LastImportedYear"],
+        duration = aggregation[i, "Duration"],
+        last.analysed.year = aggregation[i, "LastAnalysedYear"],
+        analysis.date = aggregation[i, "AnalysisDate"],
+        parent = aggregation[i, "FileFingerprint"],
+        parent.status = aggregation[i, "Status"],
+        parent.statusfingerprint = aggregation[i, "StatusFingerprint"],
+        model.fun = INLA::inla,
+        package = "INLA",
+        extractor =  function(model){
+          fe <- model$summary.fixed[, c("mean", "sd")]
+          log.index <- fe[grepl("fYear", rownames(fe)), ]
+          log.index[, "mean"] <- log.index[, "mean"] - log.index[1, "mean"]
+          log.index
+        },
+        mutate = list(cYear = "Year - max(Year)"),
+        model.args = list(family = "nbinomial")
+      )
+      store_model(object, base = analysis.path, project = "watervogels")
+      data.frame(
+        Fingerprint = get_file_fingerprint(object),
+        Parent = aggregation[i, "FileFingerprint"],
+        stringsAsFactors = FALSE
+      )
+    }
+  )
+  bind_rows(yearly, longterm)
 }
