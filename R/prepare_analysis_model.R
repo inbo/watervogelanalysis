74fc77ea651557440e06d39eaba0b9557c49f446
ThierryO <Thierry.Onkelinx@inbo.be>
2017-11-18 15:34:32 +0100

    add shortterm trend (linear trend of last 10 years)

--- R/prepare_analysis_model.R
+++ R/prepare_analysis_model.R
@@ -104,5 +104,46 @@ prepare_analysis_model <- function(
       )
     }
   )
-  bind_rows(yearly, longterm)
+  if (verbose) {
+    message("Shortterm trend")
+  }
+  shortterm <- lapply(
+    seq_along(aggregation$FileFingerprint),
+    function(i){
+      if (verbose) {
+        message("  ", aggregation[i, "FileFingerprint"])
+      }
+      object <- n2k_model_imputed(
+        result.datasource.id = aggregation[i, "ResultDatasourceID"],
+        scheme.id = aggregation[i, "SchemeID"],
+        species.group.id = aggregation[i, "SpeciesGroupID"],
+        location.group.id = aggregation[i, "LocationGroupID"],
+        model.type = "longterm imputed trend: Total ~ Year + fMonth",
+        formula = "~ cYear + f(fMonth, model = \"iid\")",
+        first.imported.year = aggregation[i, "FirstImportedYear"],
+        last.imported.year = aggregation[i, "LastImportedYear"],
+        duration = aggregation[i, "Duration"],
+        last.analysed.year = aggregation[i, "LastAnalysedYear"],
+        analysis.date = aggregation[i, "AnalysisDate"],
+        parent = aggregation[i, "FileFingerprint"],
+        parent.status = aggregation[i, "Status"],
+        parent.statusfingerprint = aggregation[i, "StatusFingerprint"],
+        model.fun = INLA::inla,
+        package = "INLA",
+        extractor =  function(model){
+          model$summary.fixed[, c("mean", "sd")]
+        },
+        filter = list("Year > max(Year) - 10"),
+        mutate = list(cYear = "Year - max(Year)"),
+        model.args = list(family = "nbinomial")
+      )
+      store_model(object, base = analysis.path, project = "watervogels")
+      data.frame(
+        Fingerprint = get_file_fingerprint(object),
+        Parent = aggregation[i, "FileFingerprint"],
+        stringsAsFactors = FALSE
+      )
+    }
+  )
+  bind_rows(yearly, longterm, shortterm)
 }
