8cb4d08a4a8ab1cf087d79d475493e5713e7eb9c
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2023-09-26 19:53:13 +0200

    run_analysis_git_s3() returns the commands to run the code on AWS EC2

--- inst/run_analysis_git_s3.R
+++ inst/run_analysis_git_s3.R
@@ -5,4 +5,12 @@ prepare_analysis(
   ),
   raw_repo = git2r::repository(fs::path("~", "n2k", "watervogels")),
   seed = 19790402, verbose = TRUE, knot_interval = 10
-) -> script
+) |>
+  sprintf(
+    fmt = c(
+      "export $(cat .env | xargs)", "aws s3 cp s3://%2$s/%1$s ~/watervogels.sh",
+      "chmod 711 ~/watervogels.sh"
+      ) |> paste(collapse = "\n"),
+    Sys.getenv("N2KBUCKET")
+  ) |>
+  cat()
