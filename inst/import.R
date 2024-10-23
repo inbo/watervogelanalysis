c889ce57aecfa7b95ac617ea192c8cfc89af5d64
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2024-10-23 10:49:22 +0200

    add import script for the analysis data

---
+++ inst/import.R
@@ -0,0 +1,18 @@
+sc <- c(
+  driver = "ODBC Driver 18 for SQL Server", server = "inbo-sql08-prd.inbo.be",
+  database = "W0004_00_Waterbirds", port = 1433, uid = "W0004_Reader",
+  pwd = keyring::key_get("meetnetten", username = "W0004_Reader"),
+  Encrypt = "no"
+)
+sprintf("%s=%s;", names(sc), sc) |>
+  paste(collapse = "") -> constring
+flemish_channel <- odbc::dbConnect(odbc::odbc(), .connection_string = constring)
+keyring::key_get("meetnetten", username = "walloon_repo") |>
+  git2r::repository() -> walloon_repo
+keyring::key_get("meetnetten", username = "raw_repo") |>
+  git2r::repository() -> raw_repo
+library(watervogelanalysis)
+prepare_dataset(
+  flemish_channel = flemish_channel, walloon_repo = walloon_repo,
+  raw_repo = raw_repo
+)
