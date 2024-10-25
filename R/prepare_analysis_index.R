bcc0393356255332b5f7f5e9ddd992a68787f359
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2024-10-25 10:33:41 +0200

    use moving_trend(), moving_difference() and moving_average() from n2kanalysis

--- R/prepare_analysis_index.R
+++ R/prepare_analysis_index.R
@@ -6,8 +6,9 @@
 #' @export
 #' @importFrom assertthat assert_that
 #' @importFrom dplyr arrange left_join select
-#' @importFrom n2kanalysis n2k_model_imputed get_file_fingerprint store_model
 #' @importFrom Matrix sparseMatrix
+#' @importFrom n2kanalysis get_file_fingerprint moving_average moving_difference
+#' moving_trend n2k_model_imputed store_model
 #' @importFrom rlang .data
 #' @importFrom stats aggregate setNames
 prepare_analysis_index <- function(
@@ -51,73 +52,6 @@ prepare_analysis_index <- function(
       return(NULL)
     }
     stopifnot(requireNamespace("INLA", quietly = TRUE))
-    moving_trend <- function(n_year, trend_year, first_year) {
-      trend_year <- min(n_year, trend_year)
-      trend_coef <- seq_len(trend_year) - (trend_year + 1) / 2
-      trend_coef <- trend_coef / sum(trend_coef ^ 2)
-      lc <- vapply(
-        seq_len(n_year - trend_year + 1),
-        function(i) {
-          c(rep(0, i - 1), trend_coef, rep(0, n_year - trend_year - i + 1))
-        },
-        numeric(n_year)
-      )
-      colnames(lc) <- sprintf(
-        "trend_%.1f_%i",
-        first_year + seq_len(ncol(lc)) - 1 + (trend_year - 1) / 2,
-        trend_year
-      )
-      t(lc)
-    }
-    moving_average <- function(n_year, trend_year, first_year) {
-      trend_year <- min(n_year, trend_year)
-      vapply(
-        seq_len(n_year - trend_year + 1) - 1,
-        FUN.VALUE = vector(mode = "numeric", length = n_year),
-        FUN = function(i, trend_coef, n_year) {
-          c(rep(0, i), trend_coef, rep(0, n_year - length(trend_coef) - i))
-        }, trend_coef = rep(1 / trend_year, trend_year), n_year = n_year
-      ) |>
-        `colnames<-`(
-          sprintf(
-            "average_%.1f_%i",
-            first_year + seq_len(n_year - trend_year + 1) - 1 +
-              (trend_year - 1) / 2,
-            trend_year
-          )
-        ) |>
-        t()
-    }
-    moving_difference <- function(n_year, trend_year, first_year) {
-      trend_year <- min(floor(n_year / 2), trend_year)
-      list(seq_len(n_year - 2 * trend_year + 1) - 1) |>
-        rep(2) |>
-        expand.grid() -> extra_zero
-      extra_zero <- extra_zero[
-        rowSums(extra_zero) <= n_year - 2 * trend_year,
-      ]
-      vapply(
-        seq_len(nrow(extra_zero)),
-        FUN.VALUE = vector(mode = "numeric", length = n_year),
-        FUN = function(i, trend_coef, n_year, extra_zero) {
-          c(
-            rep(0, extra_zero[i, 1]), -trend_coef,
-            rep(0, n_year - 2 * length(trend_coef) - sum(extra_zero[i, ])),
-            trend_coef, rep(0, extra_zero[i, 2])
-          )
-        }, trend_coef = rep(1 / trend_year, trend_year), n_year = n_year,
-        extra_zero = extra_zero
-      ) |>
-        `colnames<-`(
-          sprintf(
-            "difference_%.1f_%.1f_%i",
-            first_year + extra_zero[, 1] + trend_year / 2,
-            first_year + n_year - 1 - trend_year / 2 - extra_zero[, 2],
-            trend_year
-          )
-        ) |>
-        t()
-    }
     if (max(apply(model@AggregatedImputed@Imputation, 1, min)) < 5) {
       return(NULL)
     }
@@ -151,50 +85,33 @@ prepare_analysis_index <- function(
         INLA::inla.make.lincombs() |>
         setNames(paste("total:", winters)) -> lc1
     }
-    comb <- expand.grid(
-      winter1 = factor(winters), winter2 = factor(winters)
-    )
-    comb <- comb[as.integer(comb$winter1) < as.integer(comb$winter2), ]
-    comb$label <- sprintf("index: %s-%s", comb$winter2, comb$winter1)
-    nrow(comb) |>
-      seq_len() |>
-      rep(2) |>
-      sparseMatrix(
-        j = c(as.integer(comb$winter2), as.integer(comb$winter1)),
-        x = rep(c(1, -1), each = nrow(comb))
-      ) |>
-      list() |>
-      setNames("cyear") |>
-      INLA::inla.make.lincombs() |>
-      setNames(comb$label) -> lc2
     moving_trend(
-      n_year = length(winters), trend_year = 10, first_year = min(winters)
+      n_year = length(winters), duration = 10, first_year = min(winters)
     ) |>
       rbind(
         moving_trend(
-          n_year = length(winters), trend_year = 12,
-          first_year = min(winters)
+          n_year = length(winters), duration = 12, first_year = min(winters)
         ),
         moving_trend(
-          n_year = length(winters), trend_year = length(winters),
+          n_year = length(winters), duration = length(winters),
           first_year = min(winters)
         ),
         moving_difference(
-          n_year = length(winters), trend_year = 10,
-          first_year = min(winters)
+          n_year = length(winters), duration = 10, first_year = min(winters)
+        ),
+        moving_difference(
+          n_year = length(winters), duration = 1, first_year = min(winters)
         )
       ) |>
       unique() -> lc3
     INLA::inla.make.lincombs(cyear = lc3) |>
       setNames(rownames(lc3)) -> lc3
     moving_average(
-      n_year = length(winters), trend_year = 5,
-      first_year = min(winters)
+      n_year = length(winters), duration = 5, first_year = min(winters)
     ) |>
       rbind(
         moving_average(
-          n_year = length(winters), trend_year = 10,
-          first_year = min(winters)
+          n_year = length(winters), duration = 10, first_year = min(winters)
         )
       ) -> ma
     if (length(months) > 1) {
@@ -211,7 +128,7 @@ prepare_analysis_index <- function(
         INLA::inla.make.lincombs() |>
         setNames(rownames(ma)) -> lc4
     }
-    return(list(lincomb = c(lc1, lc2, lc3, lc4)))
+    return(list(lincomb = c(lc1, lc3, lc4)))
   }
   x <- n2k_model_imputed(
     result_datasource_id = aggregation@AnalysisMetadata$result_datasource_id,
