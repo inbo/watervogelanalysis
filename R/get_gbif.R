560035d6768b4e663170599b5d138698d095cf12
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2024-12-10 21:41:46 +0100

    ✨ Add get_gbif()
    Imports the vernacular names of the species from GBIF.

---
+++ R/get_gbif.R
@@ -0,0 +1,54 @@
+#' Get GBIF data for a species
+#'
+#' @param scientific The scientific name of the species
+#' @param verbose If TRUE, display progress messages
+#' @return A data frame with the vernacular names in different languages
+#' @export
+#' @importFrom assertthat assert_that is.string noNA
+#' @importFrom dplyr group_by inner_join slice_sample transmute ungroup
+#' @importFrom n2kanalysis display
+#' @importFrom rlang .data
+get_gbif <- function(
+  scientific, language = c("nld", "eng", "fra", "deu"), verbose = TRUE
+) {
+  assert_that(
+    is.string(scientific), noNA(scientific), is.character(language),
+    length(language) >= 1, noNA(language),
+    requireNamespace("rgbif", quietly = TRUE)
+  )
+  display(verbose = verbose, sprintf("Getting GBIF data for %s", scientific))
+  i <- 0
+  while (i < 10) {
+    backbone <- try(rgbif::name_backbone(scientific, class = "Aves"))
+    if (inherits(backbone, "data.frame")) {
+      break
+    }
+    i <- i + 1
+    Sys.sleep(i)
+  }
+  stopifnot(inherits(backbone, "data.frame"), nrow(backbone) == 1)
+  i <- 0
+  while (i < 10) {
+    gbif_lookup <- try(rgbif::name_lookup(backbone$scientificName))
+    if (inherits(backbone, "data.frame")) {
+      break
+    }
+    i <- i + 1
+    Sys.sleep(i)
+  }
+  stopifnot(
+    inherits(gbif_lookup, "gbif"),
+    as.character(backbone$usageKey) %in% names(gbif_lookup$names)
+  )
+  gbif_lookup$names[[as.character(backbone$usageKey)]] |>
+    inner_join(
+      data.frame(language = language), by = "language"
+    ) |>
+    group_by(.data$language) |>
+    slice_sample(n = 1) |>
+    ungroup() |>
+    transmute(
+      vernacular = .data$vernacularName, .data$language,
+      key = as.character(backbone$usageKey)
+    )
+}
