37174e071cbb6f7b967ddb12cb84ab47b2901b92
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2024-12-16 14:51:36 +0100

    ⚡️ Update run_analysis_git_s3
    Run the latest manifest

--- inst/run_analysis_git_s3.R
+++ inst/run_analysis_git_s3.R
@@ -1,3 +1,4 @@
+# prepare the analysis objects and a bash script the run the analyses in docker
 library(watervogelanalysis)
 Sys.setenv("AWS_ACCESS_KEY_ID" = keyring::key_get("n2kmonitoring-key"))
 Sys.setenv("AWS_SECRET_ACCESS_KEY" = keyring::key_get("n2kmonitoring-secret"))
@@ -8,7 +9,10 @@ keyring::key_get("meetnetten", username = "watervogels_repo") |>
   git2r::repository() |>
   prepare_analysis(
     analysis_path = analysis_path, raw_repo = _, seed = 19790402, verbose = TRUE
-) |>
+  ) -> manifest
+library(n2kanalysis)
+basename(manifest$Contents$Key) |>
+  read_manifest(base = analysis_path, project = "watervogels") |>
   store_manifest_yaml(
     base = analysis_path, project = "watervogels",
     docker = "inbobmk/rn2k:dev-0.10",
@@ -32,20 +36,24 @@ keyring::key_get("meetnetten", username = "watervogels_repo") |>
   ) |>
   cat(sep = "\n")
 
+# run the analyses in R
 library(n2kanalysis)
 Sys.setenv("AWS_ACCESS_KEY_ID" = keyring::key_get("n2kmonitoring-key"))
 Sys.setenv("AWS_SECRET_ACCESS_KEY" = keyring::key_get("n2kmonitoring-secret"))
 Sys.setenv("AWS_DEFAULT_REGION" =  keyring::key_get("n2kmonitoring-region"))
 keyring::key_get("n2kmonitoring-bucket") |>
   aws.s3::get_bucket(prefix = "watervogels", max = 1) -> base
-keyring::key_get("n2kmonitoring-bucket") |>
-  aws.s3::get_bucket_df(prefix = "watervogels/manifest") -> aws_objects
-aws_objects[which.max(as.POSIXct(aws_objects$LastModified)), "Key"] |>
+aws_objects <- keyring::key_get("n2kmonitoring-bucket") |>
+  aws.s3::get_bucket_df(prefix = "watervogels/manifest", max = Inf)
+as.POSIXct(aws_objects$LastModified, format = "%Y-%m-%dT%H:%M:%S.0") |>
+  which.max() -> most_recent
+aws_objects[most_recent, "Key"] |>
   basename() |>
   fit_model(
-    base = base, project = "watervogels", status = c("new", "waiting")
+    base = base, project = "watervogels", status = c("new", "waiting", "error")
   )
 
+# extract the results
 library(watervogelanalysis)
 Sys.setenv("AWS_ACCESS_KEY_ID" = keyring::key_get("n2kmonitoring-key"))
 Sys.setenv("AWS_SECRET_ACCESS_KEY" = keyring::key_get("n2kmonitoring-secret"))
@@ -57,7 +65,12 @@ keyring::key_get("meetnetten", username = "watervogels_repo") |>
 keyring::key_get("meetnetten", username = "watervogels_result") |>
   git2r::repository() -> root
 project <- "watervogels"
-extract_results(
-  x = "fdf76f77481d3e879250f10cc54ba75255cd8334", base = base,
-  project = project, raw_data = raw_data, root = root, random = FALSE
-)
+aws_objects <- keyring::key_get("n2kmonitoring-bucket") |>
+  aws.s3::get_bucket_df(prefix = "watervogels/manifest", max = Inf)
+as.POSIXct(aws_objects$LastModified, format = "%Y-%m-%dT%H:%M:%S.0") |>
+  which.max() -> most_recent
+aws_objects[most_recent, "Key"] |>
+  basename() |>
+  extract_results(
+    base = base, project = project, raw_data = raw_data, root = root
+  )
