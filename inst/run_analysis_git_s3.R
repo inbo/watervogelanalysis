93e2fbe03fc990e18887e86d34ad1c329dc7a0be
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2025-12-01 10:38:17 +0100

    ✨ Update the analysis script

--- inst/run_analysis_git_s3.R
+++ inst/run_analysis_git_s3.R
@@ -14,23 +14,12 @@ keyring::key_get("n2kmonitoring-bucket") |>
 keyring::key_get("meetnetten", username = "watervogels_repo") |>
   git2r::repository() |>
   prepare_analysis(
-    analysis_path = analysis_path, raw_repo = _, seed = 19790402, verbose = TRUE
+    analysis_path = analysis_path, seed = 19790402, verbose = TRUE
   ) -> manifest
 library(n2kanalysis)
-basename(manifest$Contents$Key) |>
-  read_manifest(base = analysis_path, project = "watervogels") |>
-  store_manifest_yaml(
-    base = analysis_path, project = "watervogels",
-    docker = "inbobmk/rn2k:dev-0.10",
-    dependencies = c(
-      "inbo/multimput@hotfix", "inbo/n2khelper@v0.5.0",
-      "inbo/n2kanalysis@spde"
-    )
-  ) |>
-  basename() |>
-  manifest_yaml_to_bash(
-    base = analysis_path, project = "watervogels", shutdown = TRUE
-  ) |>
+manifest_yaml_to_bash(
+  base = analysis_path, project = "watervogels", shutdown = TRUE
+) |>
   sprintf(
     fmt = c(
       "", "cd ~", "export $(cat .env | xargs)",
@@ -41,6 +30,36 @@ basename(manifest$Contents$Key) |>
     keyring::key_get("n2kmonitoring-bucket")
   ) |>
   cat(sep = "\n")
+
+manifest_yaml_to_bash(
+  base = analysis_path, project = "watervogels", shutdown = TRUE,
+  status = c("new", "waiting", "error"), timeout = 8
+) |>
+  sprintf(
+    fmt = c(
+      "", "cd ~", "export $(cat .env | xargs)",
+      "aws s3 cp s3://%2$s/%1$s watervogels.sh", "chmod 711 watervogels.sh", ""
+    ) |>
+      paste(collapse = "\n"),
+    keyring::key_get("n2kmonitoring-bucket")
+  ) |>
+  cat(sep = "\n")
+
+manifest_yaml_to_bash(
+  base = analysis_path, project = "watervogels", shutdown = TRUE,
+  status = "error", timeout = 8
+) |>
+  sprintf(
+    fmt = c(
+      "", "cd ~", "export $(cat .env | xargs)",
+      "aws s3 cp s3://%2$s/%1$s watervogels.sh", "chmod 711 watervogels.sh",
+      ""
+    ) |>
+      paste(collapse = "\n"),
+    keyring::key_get("n2kmonitoring-bucket")
+  ) |>
+  cat(sep = "\n")
+
 
 # run the analyses in R
 library(n2kanalysis)
@@ -61,9 +80,15 @@ aws_objects[most_recent, "Key"] |>
 
 # extract the results
 library(watervogelanalysis)
-Sys.setenv("AWS_ACCESS_KEY_ID" = keyring::key_get("n2kmonitoring-key"))
-Sys.setenv("AWS_SECRET_ACCESS_KEY" = keyring::key_get("n2kmonitoring-secret"))
-Sys.setenv("AWS_DEFAULT_REGION" =  keyring::key_get("n2kmonitoring-region"))
+if (Sys.getenv("AWS_ACCESS_KEY_ID") == "") {
+  Sys.setenv("AWS_ACCESS_KEY_ID" = keyring::key_get("n2kmonitoring-key"))
+}
+if (Sys.getenv("AWS_SECRET_ACCESS_KEY") == "") {
+  Sys.setenv("AWS_SECRET_ACCESS_KEY" = keyring::key_get("n2kmonitoring-secret"))
+}
+if (Sys.getenv("AWS_DEFAULT_REGION") == "") {
+  Sys.setenv("AWS_DEFAULT_REGION" =  keyring::key_get("n2kmonitoring-region"))
+}
 keyring::key_get("n2kmonitoring-bucket") |>
   aws.s3::get_bucket(prefix = "watervogels/manifest", max = 1) -> base
 keyring::key_get("meetnetten", username = "watervogels_repo") |>
@@ -71,12 +96,8 @@ keyring::key_get("meetnetten", username = "watervogels_repo") |>
 keyring::key_get("meetnetten", username = "watervogels_result") |>
   git2r::repository() -> root
 project <- "watervogels"
-aws_objects <- keyring::key_get("n2kmonitoring-bucket") |>
-  aws.s3::get_bucket_df(prefix = "watervogels/manifest", max = Inf)
-as.POSIXct(aws_objects$LastModified, format = "%Y-%m-%dT%H:%M:%S.0") |>
-  which.max() -> most_recent
-aws_objects[most_recent, "Key"] |>
-  basename() |>
+read_manifest(base = base, project = project) |>
+  slot("Fingerprint") |>
   extract_results(
     base = base, project = project, raw_data = raw_data, root = root
   )
